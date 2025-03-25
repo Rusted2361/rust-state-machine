5c5fafa5a958034541da44265c7516907985fea0
Rusted2361 <hamzasajid4040@gmail.com>
2025-03-25 23:58:02 +0500

    made types generic

--- src/balances.rs
+++ src/balances.rs
@@ -1,18 +1,20 @@
 use std::collections::BTreeMap;
-
-type AccountId = String;
-type Balance = u128;
+use num::traits::{CheckedAdd, CheckedSub, Zero};
 
 /// This is the Balances Module.
 /// It is a simple module which keeps track of how much balance each account has in this state
 /// machine.
 #[derive(Debug)]
-pub struct Pallet {
+pub struct Pallet<AccountId, Balance> {
 	// A simple storage mapping from accounts (`String`) to their balances (`u128`).
 	balances: BTreeMap<AccountId, Balance>,
 }
 
-impl Pallet {
+impl<AccountId, Balance> Pallet<AccountId, Balance> 
+where
+	AccountId: Ord + Clone,
+    Balance: Zero + CheckedSub + CheckedAdd + Copy,	
+{
 	/// Create a new instance of the balances module.
 	pub fn new() -> Self {
 		Self { balances: BTreeMap::new() }
@@ -26,7 +28,7 @@ impl Pallet {
 	/// Get the balance of an account `who`.
 	/// If the account has no stored balance, we return zero.
 	pub fn balance(&self, who: &AccountId) -> Balance {
-		*self.balances.get(who).unwrap_or(&0)
+		*self.balances.get(who).unwrap_or(&Balance::zero())
 	}
 
 	pub fn transfer(
@@ -38,8 +40,8 @@ impl Pallet {
 		let caller_balance = self.balance(&caller);
 		let to_balance = self.balance(&to);
 
-		let new_caller_balance = caller_balance.checked_sub(amount).ok_or("Not enough funds.")?;
-		let new_to_balance = to_balance.checked_add(amount).ok_or("Overflow")?;
+		let new_caller_balance = caller_balance.checked_sub(&amount).ok_or("Not enough funds.")?;
+		let new_to_balance = to_balance.checked_add(&amount).ok_or("Overflow")?;
 
 		self.balances.insert(caller, new_caller_balance);
         self.balances.insert(to, new_to_balance);
@@ -53,7 +55,7 @@ mod tests {
 	#[test]
 	fn init_balances() {
 		/* TODO: Create a mutable variable `balances`, which is a new instance of `Pallet`. */
-		let mut balances = crate::balances::Pallet::new();
+		let mut balances = crate::balances::Pallet::<String, u128>::new();
 
 		/* TODO: Assert that the balance of `alice` starts at zero. */
 		assert!(balances.balance(&"alice".to_string()) == 0);
@@ -67,7 +69,7 @@ mod tests {
 
 	#[test]
     fn transfer_balance() {
-        let mut balances = super::Pallet::new();
+        let mut balances = super::Pallet::<String, u128>::new();
 
         assert_eq!(
             balances.transfer("alice".to_string(), "bob".to_string(), 51),
--- src/system.rs
+++ src/system.rs
@@ -1,28 +1,35 @@
-/* TODO: You might need to update your imports. */
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, ops::AddAssign};
+
+use num::{zero, One, Zero};
+
 /// This is the System Pallet.
 /// It handles low level state needed for your blockchain.
 #[derive(Debug)]
-pub struct Pallet {
+pub struct Pallet<AccountId, BlockNumber, Nonce> {
 	/// The current block number.
 	/* TODO: Create a field `block_number` that stores a `u32`. */
-    block_number: u32,
+    block_number: BlockNumber,
 	/// A map from an account to their nonce.
 	/* TODO: Create a field `nonce` that is a `BTreeMap` from `String` to `u32`. */
-    nonce: BTreeMap<String, u32>,
+    nonce: BTreeMap<AccountId, Nonce>
 }
 
-impl Pallet {
+impl<AccountId,BlockNumber, Nonce> Pallet<AccountId, BlockNumber, Nonce>
+where 
+    AccountId: Ord + Clone,
+    BlockNumber:Zero + AddAssign + Copy + One, 
+    Nonce:Zero + One + Copy
+{
 	/// Create a new instance of the System Pallet.
 	pub fn new() -> Self {
 		/* TODO: Return a new instance of the `Pallet` struct. */
-        Self{block_number: 0,
+        Self{block_number: BlockNumber::zero(),
         nonce: BTreeMap::new(),}
         
 	}
 
 	/// Get the current block number.
-    pub fn block_number(&self) -> u32 {
+    pub fn block_number(&self) -> BlockNumber {
         /* TODO: Return the current block number. */
         self.block_number
     }
@@ -31,21 +38,23 @@ impl Pallet {
     // Increases the block number by one.
     pub fn inc_block_number(&mut self) {
         /* TODO: Increment the current block number by one. */
-        self.block_number += 1;
+        self.block_number += BlockNumber::one();
     }
 
     // Increment the nonce of an account. This helps us keep track of how many transactions each
     // account has made.
-    pub fn inc_nonce(&mut self, who: &String) {
+    pub fn inc_nonce(&mut self, who: &AccountId) {
         /* TODO: Get the current nonce of `who`, and increment it by one. */
-        let nonce: u32 = *self.nonce.get(who).unwrap_or(&0);
-		let new_nonce = nonce + 1;
+        let nonce: Nonce = *self.nonce.get(who).unwrap_or(&zero());
+		let new_nonce = nonce + Nonce::one();
 		self.nonce.insert(who.clone(), new_nonce);
     }
 }
 
 #[cfg(test)]
 mod test {
+    // use crate::types::{AccountId, BlockNumber, Nonce};
+
     #[test]
     fn init_system() {
         /* TODO: Create a test which checks the following:
@@ -57,7 +66,7 @@ mod test {
             - Check the nonce of `bob` is what we expect.
         */
 
-		let mut system = super::Pallet::new();
+		let mut system = super::Pallet::<String, u32, u32>::new();
         system.inc_block_number();
         system.inc_nonce(&"alice".to_string());
 
